02275bd58869fdebe8b42f09371aec4db1cd1612
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2024-04-16 10:57:08 -0400

    Adding Recent Transaction Card

--- ExpenseTracker/RecentTransactionList.swift
+++ ExpenseTracker/RecentTransactionList.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct RecentTransactionList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack {
+        HStack {
+          // MARK: Header Title
+          Text("Recent Transactions")
+            .bold()
+
+          Spacer()
+
+          // MARK: Header Link
+          NavigationLink {
+
+          } label: {
+            HStack(spacing: 4) {
+              Text("See All")
+              Image(systemName: "chevron.right")
+            }
+            .foregroundColor(Color.text)
+          }
+        }
+        .padding(.top)
+      }
+      .padding()
+      .background(Color.systemBackground)
+      .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+      .shadow(color: Color.primary.opacity(0.2), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 0, y: 5)
     }
 }
 
